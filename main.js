7bf83c53a53ec3ad9093aaea3797b2edc6e4d022
Malev1ch <130825280+Malev1ch@users.noreply.github.com>
2023-04-27 18:28:41 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -25,26 +25,16 @@
 //Приветствие после ввода данных
 
 
+
 //светофор
-// const colorList = [               
-//     {name: 'yellow', interval: 500},
-//     {name: 'red', interval: 4000},
-//     {name: 'yellow', interval: 500},
-//     {name: 'green', interval: 4000},
-//     {name: 'you can cross the traffic light', interval: 100},
-//   ];
-  
-//   let count = 0;
-  
-//   function changeColor() {
-//     if (count === colorList.length) {
-//       count = 0;
-//     }
-//     console.log(colorList[count].name)
-//     setTimeout(changeColor, colorList[count].interval)
-//     count = count + 1;
-//   }
-  
-//   changeColor()
-  
+// const color = prompt("Задайте из следующий цветов  красный , жёлтый и зелёный")
+
+// if (color === "красный") {
+//     alert("стоять")
+// }else if (color === "жёлтый") {
+//     alert("ждать")
+// }else if (color === "жёлтый") {
+//     alert("ждать")
+// }  alert( "Введите цвет из списка (красный, желтый, зеленый)" ); 
+
 //светофор
