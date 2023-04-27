887f64b967628e848f94e1aa222ca99962341360
Malev1ch <130825280+Malev1ch@users.noreply.github.com>
2023-04-27 16:59:04 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,50 @@
-//Приветствие после ввода данных
-const userName = prompt("Enter your Name");
-const userSecondName = prompt("Enter your second Name")
+//сравнение чисел
 
-let congratulation = userName + " " + userSecondName;
-alert("Hello, dear:" + congratulation)
+// var a = prompt (" введите число", ""); 
+// var b = prompt (" введите число", "");
+//  if (a>b)
+//  { alert ('Первое число больше второго');}
+//   if (a<b)
+// { alert ('Второе число больше первого');}
+ 
+//  if (a==b)
+//  {alert ('Числа равны');}
+
+//сравнение чисел
+
+
 
 //Приветствие после ввода данных
+
+// const userName = prompt("Enter your Name");
+// const userSecondName = prompt("Enter your second Name")
+
+// let congratulation = userName + " " + userSecondName;
+// alert("Hello, dear:" + congratulation)
+
+//Приветствие после ввода данных
+
+
+//светофор
+// const colorList = [               
+//     {name: 'yellow', interval: 500},
+//     {name: 'red', interval: 4000},
+//     {name: 'yellow', interval: 500},
+//     {name: 'green', interval: 4000},
+//     {name: 'you can cross the traffic light', interval: 100},
+//   ];
+  
+//   let count = 0;
+  
+//   function changeColor() {
+//     if (count === colorList.length) {
+//       count = 0;
+//     }
+//     console.log(colorList[count].name)
+//     setTimeout(changeColor, colorList[count].interval)
+//     count = count + 1;
+//   }
+  
+//   changeColor()
+  
+//светофор
