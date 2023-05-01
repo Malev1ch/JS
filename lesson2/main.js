7d87d50814bb6501ae7a86f45c5b17f6659ce87d
Malev1ch <130825280+Malev1ch@users.noreply.github.com>
2023-05-01 19:17:17 +0600

    Add files via upload

--- lesson2/main.js
+++ lesson2/main.js
@@ -1,1 +1,87 @@
+//Triangle
 
+// var triangle = "#";
+
+// do {
+//     console.log(triangle);
+//     triangle += "#";
+// } while (triangle.length <= 7);
+
+//Triangle
+
+//FizzBuzz
+
+// for (var i = 1; i <= 100; i++) {
+//     if ( !(i % 3) && !(i % 5) ) {
+//         console.log(i, "FizzBuzz");
+//     } else if ( !(i % 3) ) {
+//         console.log(i, "Fizz");
+//     } else if ( !(i % 5) ) {
+//         console.log(i, "Buzz");
+//     }
+// }
+
+//FizzBuzz
+
+//chessboard
+
+// var chessboard = 8,
+//     totalSize = chessboard * chessboard,
+//     line = "",
+//     flag = true;
+
+
+// function createLine(chessboard, flag) {
+//     var result = "";
+//     for (var i = 0; i < chessboard; i++) {
+//         (i % 2) ? result += "#" : result += " ";
+//     }
+//     if (flag) {
+//         result = reverseString(result);
+//     }
+//     return result;
+// }
+
+// function reverseString(str) {
+//     var result = "";
+//     for (var i = str.length - 1; i > 0; i--) {
+//         result += str[i];
+//     }
+//     return result;
+// }
+
+
+// for (var i = 1; i <= chessboard; i++) {
+//     if (flag) {
+//         flag = false;
+//     } else {
+//         flag = true;
+//     }
+//     console.log(createLine(chessboard, flag));
+// }
+
+//chessboard
+
+//4dop
+// const mass = ['Alex', 'Anna', 'Oleg', '1', '2', '3', true]
+// console.log(mass);
+//4dop
+
+//5dop
+// let array =  ['Алина', 'Саша', 'Алекс'];
+// console.log(array [0]);
+//5dop
+
+//6dop
+// let arr1 = [1, true, false, undefined];
+// let arr2 = ['Sasha', 'Alina', false, undefined];
+
+// for (let i = 0; i < arr2.length; i++) {
+//   if (arr2[i] !== undefined) {
+//     arr1[i] = arr2[i];
+//   }
+// }
+
+// console.log(arr1);
+
+//6dop
